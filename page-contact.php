d4ffb7735c7427e0a42b4e64a1081e852170fcb7
FatiSan <148770863+FatiSan@users.noreply.github.com>
2024-09-27 18:01:30 +0100

    Update page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -1,5 +1,95 @@
-<?php include 'header.php';?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Experts en Menuiserie Aluminium au Maroc - Kech Trav Alum</title>
+    <meta name="description" content="">
+    <meta name="description" content="Kech Trav Alum Groupe: Expert en menuiserie aluminium au Maroc. Solutions sur mesure pour portes, fenêtres et façades. Qualité et durabilité garanties">
+        <meta name="author" content="themsflat.com" />
+    <meta name="google-site-verification" content="UJHwg0YbZZGSKa_jrGNO-d_2eZtotV6lW0C4aHWwNk0" />
+    <meta name="keywords" content="menuisier aluminium Maroc,menuiserie aluminium Maroc,solutions aluminium élégantes,aluminium durable,chef-d&#x27;œuvre en aluminium,kech trav alum groupe,aluminium Marrakech,artisan aluminium Maroc,construction aluminium Maroc,aluminium sur mesure,solutions aluminium sur mesure,menuisier aluminium Marrakech,entreprise aluminium Maroc,transformation aluminium,aluminium de qualité,design aluminium Maroc,aluminium pour intérieur,aluminium pour extérieur,aluminium durable et élégant,menuiserie sur mesure Maroc">
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport"  content="width=device-width, initial-scale=1, maximum-scale=1" />
 
+
+    <!-- Theme style -->
+    <link rel="stylesheet" type="text/css" href="stylesheet/style.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheet/shortcodes.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheet/color.css" />
+    <!-- Animation -->
+    <link rel="stylesheet" type="text/css" href="stylesheet/animate.css" />
+
+    <!-- Reponsive -->
+    <link rel="stylesheet" type="text/css" href="stylesheet/responsive.css" />
+
+    <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
+       <!-- Favicon and touch icons  -->
+       <link href="images/Icon/Favicon.png" rel="apple-touch-icon-precomposed">
+       <link href="images/Icon/Favicon.png" rel="apple-touch-icon-precomposed">
+       <link href="images/Icon/Favicon.png" rel="shortcut icon">
+  </head>
+  <body class="counter-scroll header-fixed header_sticky header-absolute">
+    <div class="animsition boxed blog">
+        <div class="top-bar-style2 clearfix" >
+            <div class="container-fluid">
+                <ul class="top-content">
+                    <li class="adress">
+                      <a href="#"><i class="icon-iconmap"></i>Harbile à côté de almojazine tamansourte, Marrakech 400000</a>
+                    </li>
+                    <li class="email"><i class="icon-iconmail"></i> <a href="#">Aluminium_bendra@hotmail.com</a></li>
+                  </ul>
+  
+                <div class="topbar-socials">
+                  <span class="icons">
+                    <a href="https://www.instagram.com/societe_kech_trav_alum_groupe/"><i class="fab fa-instagram"></i></a>
+                  </span>
+                </div>
+            </div>    
+        </div>
+        <header class="header-style2 bg-color-style2 bg-color" id="header">
+            <div class="container-fluid container-fixel">
+              <div class="flex-header d-lg-flex">
+                     <div class="logo logo-style2">
+                        <a href="images/logo.png"> <img width="10%"  src="images/logo.png"   data-retina="images/logo.png" alt="images"></a>
+                        
+                     </div>
+                     <div class="content-menu d-lg-flex">
+                    <div class="nav-wrap">
+                        <div class="btn-menu">
+                          <span></span>
+                        </div>
+                        <nav id="mainnav" class="mainnav fixed-mainnav">
+                          <ul class="menu menustyle2">
+                            <li class=" current-menu current-menu-item current-menu-ancestor ">
+                                <a href="index.html">Home</a>
+                               
+                            </li>
+                            <li><a href="page-window.html">Windows</a>
+                            </li>
+                            <li><a href="page-door.php">Doors</a>
+                            </li>
+                            <li><a href="page-gallery.html">Gallery</a>
+                            </li>
+                             <li><a href="page-contact.html">Contact</a></li>                      
+                        </ul>
+                          <!-- /.menu -->
+                        </nav>
+                        <!-- /#mainnav -->
+                      </div>
+                      <!-- /.nav-wrap -->
+
+                      <div class="search-btn">
+                          <div class="btn-top">
+                              <a href="https://wa.me/0661546065" class="themesflat-button-style2 btn-style-4">
+                              <span class="btn-title">Contact Us</span>
+                          </a>
+                          </div>
+                         	<!-- /.flat-show-search -->
+                      </div>
+                  </div>
+                </div>
+            </div>
+        </header>
         <div class="page-title page-title-inner padding-bottom9">
             <div class="overlay-page-tile"></div>
             <div class="page-title-content">
